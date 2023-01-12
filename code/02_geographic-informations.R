6ee65413d12695bcdc819a843a79e5ff60330423
JWicquart <jeremywicquart@gmail.com>
2023-01-12 17:59:42 +0100

    export geographic informations from R to LaTeX

--- code/02_geographic-informations.R
+++ code/02_geographic-informations.R
@@ -2,20 +2,59 @@
 
 library(tidyverse)
 library(sf)
+sf_use_s2(FALSE)
 
 # 2. Source functions ----
 
 source("code/function/export_tabular.R")
 
-# 2. Maritime area ----
+# 3. Maritime area ----
+
+load("data/02_eez/data_eez.RData")
+
+data_maritime_area <- data_eez %>% 
+  st_drop_geometry() %>% 
+  select(TERRITORY1, AREA_KM2) %>% 
+  group_by(TERRITORY1) %>% 
+  summarise(AREA_KM2 = sum(AREA_KM2)) %>% 
+  ungroup() %>% 
+  mutate(AREA_KM2 = format(AREA_KM2, big.mark = ","))
+
+# 4. Reef area ----
+
+data_reefs <- st_read("data/03_reefs-area_wri/reef_500_poly.shp") %>% 
+  st_transform(crs = 4326) %>% 
+  st_wrap_dateline(options = c("WRAPDATELINE=YES"))
 
 load("data/02_eez/data_eez.RData")
 
 data_eez <- data_eez %>% 
-  st_drop_geometry() %>% 
-  select(TERRITORY1, AREA_KM2)
+  st_transform(crs = 4326) %>% 
+  st_wrap_dateline(options = c("WRAPDATELINE=YES"))
 
-# 3. Export 
+ggplot() +
+  geom_sf(data = data_eez) +
+  geom_sf(data = data_reefs, color = "red")
+
+data_reef_area <- st_intersection(data_eez, data_reefs) %>%  
+  group_by(TERRITORY1) %>% 
+  summarise(area = sum(st_area(geometry))) %>% 
+  st_drop_geometry() %>% 
+  mutate(area = area*1e-6,
+         area = as.numeric(area),
+         area = round(area, 0),
+         area = format(area, big.mark = ","))
+
+# 5. Human population ----
+
+data_population <- read.csv("data/06_geo-inf/01_human-pop.csv") %>% 
+  rename(population = sum) %>% 
+  group_by(TERRITORY1) %>% 
+  summarise(population = sum(population)) %>% 
+  ungroup() %>% 
+  mutate(population = round(population, 0),
+         population = format(population, big.mark = ",", scientific = FALSE))
+
+# 6. Export 
 
 map(unique(data_eez$TERRITORY1), ~export_tabular(territory_i = .))
-
--- code/function/export_tabular.R
+++ code/function/export_tabular.R
@@ -1,9 +1,25 @@
 export_tabular <- function(territory_i){
   
+  data_maritime_area_i <- data_maritime_area %>% 
+    filter(TERRITORY1 == territory_i) %>% 
+    select(AREA_KM2) %>% 
+    pull()
+  
+  data_reef_area_i <- data_reef_area %>% 
+    filter(TERRITORY1 == territory_i) %>% 
+    select(area) %>% 
+    pull()
+  
+  data_population_i <- data_population %>% 
+    filter(TERRITORY1 == territory_i) %>% 
+    select(population) %>% 
+    pull()
+  
   writeLines(c("\\begin{tabular}{>{\\bfseries}>{\\color{color1}}rl}",
-               "Maritime area & 120,000 km\\textsuperscript{2} \\\\",
+               paste0("Maritime area & ", data_maritime_area_i, " km\\textsuperscript{2} \\\\"),
                "Land area & 12,000 km\\textsuperscript{2} \\\\",
-               "Reef area & 500 km\\textsuperscript{2} \\\\",
+               paste0("Reef area & ", data_reef_area_i, " km\\textsuperscript{2} \\\\"),
+               paste0("Population (2020) & ", data_population_i, " \\\\"),
                "\\end{tabular}"),
              paste0("figs/02_geographic-inf_", str_replace_all(str_to_lower(territory_i), " ", "-"), ".tex"))
   
