965301b7a39301bfaafd27aa81a2e0985e6ead12
JWicquart <jeremywicquart@gmail.com>
2023-03-09 17:43:05 +0100

    exclude some EEZ

--- code/02_individual-maps.R
+++ code/02_individual-maps.R
@@ -19,10 +19,11 @@ load("data/01_background-shp/03_eez/data_eez.RData")
 
 data_parameters <- read.csv2("data/path_individual_maps.csv")
 
-data_benthic <- load("C:/Users/jwicquart/Desktop/Recherche/03_projects/2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/09_gcrmndb_benthos.RData")
+load("data/04_data-benthic.RData")
 
-data_benthic <- synthetic_data %>% 
-  filter(higherGeography == "Pacific") %>% 
+data_benthic <- data_benthic %>% 
+  select(decimalLatitude, decimalLongitude, territory) %>% 
+  distinct() %>% 
   st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
 
 # 4. Create the EEZ individual maps ----
@@ -51,6 +52,20 @@ b <- b %>%
   st_as_sfc() %>% 
   st_transform(., "+proj=ortho +lat_0=0 +lon_0=-175")
 
-# 5.3 Produce all the maps --
+# 5.3 Change EEZ levels for Kiribati and Pacific Remote Island Area -- 
+
+data_eez <- data_eez %>% 
+  # Kiribati
+  mutate(TERRITORY1 = str_replace_all(TERRITORY1, c("Phoenix Group" = "Kiribati",
+                                                    "Gilbert Islands" = "Kiribati",
+                                                    "Line Group" = "Kiribati"))) %>% 
+  # Pacific Remote Island Area
+  mutate(TERRITORY1 = str_replace_all(TERRITORY1, c("Jarvis Island" = "Pacific Remote Island",
+                                                     "Wake Island" = "Pacific Remote Island",
+                                                     "Howland and Baker islands" = "Pacific Remote Island",
+                                                     "Johnston Atoll" = "Pacific Remote Island",
+                                                     "Palmyra Atoll" = "Pacific Remote Island")))
+
+# 5.4 Produce all the maps --
 
 map(unique(data_eez$TERRITORY1), ~map_ortho(territory_i = .))
