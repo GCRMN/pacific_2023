064759caabf517a921c7acb39f314cc43ed552b5
JWicquart <jeremywicquart@gmail.com>
2023-03-17 11:29:32 +0100

    extract elevation for each territory on GEE

---
+++ code/02_extract_elevation.js
@@ -0,0 +1,22 @@
+// 1. Import elevation data ----
+
+var elevation = ee.Image('CGIAR/SRTM90_V4').select('elevation');
+
+// 2. Extract maximum elevation ----
+
+var data_elevation = elevation.reduceRegions({
+  reducer: ee.Reducer.max(),
+  collection: data_eez,
+  scale: 90,
+});
+
+// 3. Export the data ----
+
+Export.table.toDrive({
+  collection:data_elevation,
+  folder:"GEE",
+  fileNamePrefix:"02_elevation",
+  fileFormat:"CSV",
+  description:"02_elevation",
+  selectors:["GEONAME", "max"]
+});
