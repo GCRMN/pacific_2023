72b82ac4fcfb2018011eb86cf8373157fea28993
JWicquart <jeremywicquart@gmail.com>
2023-03-30 15:06:43 +0200

    modify theme and correct cyclones path for eez crossing 180 line

--- code/03_territories_cyclones.R
+++ code/03_territories_cyclones.R
@@ -4,6 +4,9 @@ library(tidyverse) # Core tidyverse packages
 library(lubridate)
 library(sf)
 sf_use_s2(FALSE)
+library(ggrepel)
+library(glue)
+library(scales)
 
 # 2. Source functions ----
 
@@ -19,47 +22,62 @@ load("data/05_cyclones/02_cyclones_extracted.RData")
 load("data/05_cyclones/01_cyclones_lines.RData")
 load("data/05_cyclones/01_cyclones_points.RData")
 
+# 4. Transform data ----
+
+n_cyclones <- 10 # Number of cyclones label to show on plots
+
 data_cyclones <- data_cyclones %>% 
+  group_by(territory) %>% 
+  arrange(desc(wind_speed)) %>% 
+  mutate(pos = row_number()) %>% 
+  ungroup() %>% 
   mutate(time = as_date(time),
-         ts_dist = round(ts_dist, 1))
+         ts_dist = round(ts_dist, 1),
+         # 13 is the maximum number of labels to show
+         name_a = if_else(pos <= n_cyclones, glue('"{name}" * phantom(" ({ts_dist} km)")'), NA),
+         name_b = if_else(pos <= n_cyclones, glue('phantom("{name}") * " ({ts_dist} km)"'), NA))
 
-# 4. Plot of cyclones occurrence over time ----
+# NOTE - A combination of geom_text_repel() with geom_richtext() doesn't exist
+# I use the solution proposed on https://github.com/slowkow/ggrepel/issues/145
 
-# 4.1 Create the function --
+# 5. Plot of cyclones occurrence over time ----
+
+# 5.1 Create the function --
 
 map_cyclone_plot <- function(territory_i){
   
-  ggplot(data = data_cyclones %>% filter(territory == territory_i), aes(x = time, y = wind_speed)) +
-    geom_point(color = "white", shape = 21, fill = "red", size = 4) +
-    geom_text(aes(y = wind_speed + 22, label = name), size = 3, family = font_choose_graph, col = "#d91e18") +
-    geom_text(aes(y = wind_speed + 12, label = paste0(ts_dist, " km")), size = 2, family = font_choose_graph, col = "#2e3131") +
+  ggplot(data = data_cyclones %>% filter(territory == territory_i), 
+         aes(x = time, y = wind_speed, fill = ifelse(pos <= n_cyclones, "#d91e18", "darkgrey"))) +
+    geom_text_repel(aes(y = wind_speed, label = name_a), size = 2,
+                    family = font_choose_graph, col = "#d91e18",
+                    min.segment.length = 0, segment.size = 0.25,
+                    force = 50, seed = 1, parse = TRUE) +
+    geom_text_repel(aes(y = wind_speed, label = name_b), size = 2,
+                    family = font_choose_graph, col = "black",
+                    min.segment.length = 0, segment.size = 0.25,
+                    force = 50, seed = 1, parse = TRUE) +
+    geom_point(color = "white", shape = 21, size = 3, show.legend = FALSE) +
     lims(x = c(ymd("1970-01-01"), ymd("2030-01-01"))) +
-    labs(x = "Time (years)", y = bquote("Wind speed (km."~h^-1*")"), title = "B") +
-    coord_cartesian(expand = FALSE, clip = "on") +
+    labs(x = "Year", y = bquote("Wind speed (km."~h^-1*")")) +
+    scale_fill_identity() +
     scale_y_continuous(breaks = c(0, 50 ,100, 150, 200, 250, 300), limits = c(-20, 300)) +
-    theme(text = element_text(family = font_choose_graph),
-          panel.grid.major.y = element_line(color = "lightgrey", linewidth = 0.3),
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          panel.grid.minor.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.ticks.length = unit(4, "pt"),
-          axis.line.x = element_line(color = "black", linewidth = 0.5),
-          panel.background = element_blank(),
-          plot.margin = margin(t = 5, r = 15, b = 5, l = 5, unit = "pt"))
+    scale_x_date(breaks = c(as.Date(c("1970-01-01", "1980-01-01", "1990-01-01", "2000-01-01", "2010-01-01", "2020-01-01", "2030-01-01"))),
+                 labels=date_format("%Y"),
+                 limits = as.Date(c("1970-01-01", "2030-01-01"))) +
+    theme_graph()
   
   ggsave(filename = paste0("figs/territories_fig-3/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
          width = 6, height = 4, dpi = 600)
   
 }
 
-# 4.2 Map over the function --
+# 5.2 Map over the function --
 
 map(unique(data_cyclones$territory), ~map_cyclone_plot(territory_i = .))
 
-# 5. Map of cyclone occurrence over EEZ ----
+# 6. Map of cyclone occurrence over EEZ ----
 
-# 5.1 Create the function --
+# 6.1 Create the function --
 
 map_cyclone_map <- function(territory_i){
   
@@ -72,12 +90,24 @@ map_cyclone_map <- function(territory_i){
     data_land_i <- data_land %>% 
       filter(TERRITORY1 == territory_i) %>% 
       st_transform(., crs = 3460)
+    
+    data_cyclones_i <- data_cyclones %>% 
+      filter(territory == territory_i)
+    
+    data_ts_lines_i <- left_join(data_cyclones_i, data_ts_lines) %>% 
+      st_as_sf() %>% 
+      st_transform(., crs = 3460) %>% 
+      st_intersection(., data_eez_i %>% st_transform(crs = 3460))
 
     ggplot() +
       # EEZ borders
       geom_sf(data = data_eez_i, color = "black", fill = "#89c4f4", alpha = 0.75) +
       # Lands
       geom_sf(data = data_land_i, fill = "#363737", col = "grey") +
+      # Cyclone path (those > n_cyclones)
+      geom_sf(data = data_ts_lines_i %>% filter(pos > n_cyclones), col = "darkgrey") +
+      # Cyclone path (those <= n_cyclones)
+      geom_sf(data = data_ts_lines_i %>% filter(pos <= n_cyclones), col = "#d91e18") +
       theme_minimal() +
       theme(axis.text = element_blank(),
             axis.ticks = element_blank(),
@@ -98,20 +128,15 @@ map_cyclone_map <- function(territory_i){
       st_as_sf() %>% 
       st_intersection(., data_eez_i %>% st_transform(crs = 4326))
     
-    data_ts_points_i <- data_ts_points %>% 
-      filter(ts_id %in% unique(data_cyclones_i$ts_id)) %>% 
-      st_as_sf() %>% 
-      st_intersection(., data_eez_i %>% st_transform(crs = 4326))
-    
     ggplot() +
       # EEZ borders
       geom_sf(data = data_eez_i, color = "black", fill = "#89c4f4", alpha = 0.75) +
       # Lands
       geom_sf(data = data_land_i, fill = "#363737", col = "grey") +
-      # Cyclone path
-      geom_sf(data = data_ts_lines_i) +
-      # Cyclone points
-      geom_sf(data = data_ts_points_i, aes(fill = wind_speed), shape = 21, color = "black") +
+      # Cyclone path (those > n_cyclones)
+      geom_sf(data = data_ts_lines_i %>% filter(pos > n_cyclones), col = "darkgrey") +
+      # Cyclone path (those <= n_cyclones)
+      geom_sf(data = data_ts_lines_i %>% filter(pos <= n_cyclones), col = "#d91e18") +
       theme_minimal() +
       theme(axis.text = element_blank(),
             axis.ticks = element_blank(),
@@ -123,6 +148,6 @@ map_cyclone_map <- function(territory_i){
   
 }
 
-# 5.2 Map over the function --
+# 6.2 Map over the function --
 
 map(unique(data_cyclones$territory), ~map_cyclone_map(territory_i = .))
