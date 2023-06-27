ecdce5382356fa08e17b8843dc04c58307e77531
JWicquart <jeremywicquart@gmail.com>
2023-06-27 15:58:57 +0200

    update dataviz for cyclone occurence and trajectories

--- code/03_territories_cyclones.R
+++ code/03_territories_cyclones.R
@@ -1,12 +1,13 @@
 # 1. Required packages ----
 
 library(tidyverse) # Core tidyverse packages
-library(lubridate)
 library(sf)
 sf_use_s2(FALSE)
 library(ggrepel)
 library(glue)
 library(scales)
+library(lubridate)
+library(scico)
 
 # 2. Source functions ----
 
@@ -15,71 +16,16 @@ source("code/function/theme_graph.R")
 
 # 3. Load data ----
 
-data_reef <- st_read("data/03_reefs-area_wri/clean/pacific_reef.shp")
 load("data/01_background-shp/03_eez/data_eez.RData")
 load("data/01_background-shp/02_princeton/data_land.RData")
+load("data/01_background-shp/01_ne/ne_10m_bathymetry_all.RData")
 load("data/05_cyclones/02_cyclones_extracted.RData")
 load("data/05_cyclones/01_cyclones_lines.RData")
 load("data/05_cyclones/01_cyclones_points.RData")
 
-# 4. Transform data ----
+# 4. Create the function ----
 
-n_cyclones <- 10 # Number of cyclones label to show on plots
-
-data_cyclones <- data_cyclones %>% 
-  group_by(territory) %>% 
-  arrange(desc(wind_speed)) %>% 
-  mutate(pos = row_number()) %>% 
-  ungroup() %>% 
-  mutate(time = as_date(time),
-         ts_dist = round(ts_dist, 1),
-         # 13 is the maximum number of labels to show
-         name_a = if_else(pos <= n_cyclones, glue('"{name}" * phantom(" ({ts_dist} km)")'), NA),
-         name_b = if_else(pos <= n_cyclones, glue('phantom("{name}") * " ({ts_dist} km)"'), NA))
-
-# NOTE - A combination of geom_text_repel() with geom_richtext() doesn't exist
-# I use the solution proposed on https://github.com/slowkow/ggrepel/issues/145
-
-# 5. Plot of cyclones occurrence over time ----
-
-# 5.1 Create the function --
-
-map_cyclone_plot <- function(territory_i){
-  
-  ggplot(data = data_cyclones %>% filter(territory == territory_i), 
-         aes(x = time, y = wind_speed, fill = ifelse(pos <= n_cyclones, "#d91e18", "darkgrey"))) +
-    geom_text_repel(aes(y = wind_speed, label = name_a), size = 2,
-                    family = font_choose_graph, col = "#d91e18",
-                    min.segment.length = 0, segment.size = 0.25,
-                    force = 50, seed = 1, parse = TRUE) +
-    geom_text_repel(aes(y = wind_speed, label = name_b), size = 2,
-                    family = font_choose_graph, col = "black",
-                    min.segment.length = 0, segment.size = 0.25,
-                    force = 50, seed = 1, parse = TRUE) +
-    geom_point(color = "white", shape = 21, size = 3, show.legend = FALSE) +
-    lims(x = c(ymd("1970-01-01"), ymd("2030-01-01"))) +
-    labs(x = "Year", y = bquote("Wind speed (km."~h^-1*")")) +
-    scale_fill_identity() +
-    scale_y_continuous(breaks = c(0, 50 ,100, 150, 200, 250, 300), limits = c(-20, 300)) +
-    scale_x_date(breaks = c(as.Date(c("1970-01-01", "1980-01-01", "1990-01-01", "2000-01-01", "2010-01-01", "2020-01-01", "2030-01-01"))),
-                 labels=date_format("%Y"),
-                 limits = as.Date(c("1970-01-01", "2030-01-01"))) +
-    theme_graph()
-  
-  ggsave(filename = paste0("figs/territories_fig-3/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
-         width = 6, height = 4, dpi = 600)
-  
-}
-
-# 5.2 Map over the function --
-
-map(unique(data_cyclones$territory), ~map_cyclone_plot(territory_i = .))
-
-# 6. Map of cyclone occurrence over EEZ ----
-
-# 6.1 Create the function --
-
-map_cyclone_map <- function(territory_i){
+map_cyclone <- function(territory_i){
   
   if(territory_i %in% c("Fiji", "Wallis and Futuna", "Hawaii", "Tuvalu", "Gilbert Islands")){
     
@@ -91,63 +37,197 @@ map_cyclone_map <- function(territory_i){
       filter(TERRITORY1 == territory_i) %>% 
       st_transform(., crs = 3460)
     
+    data_bathy_i <- data_bathy %>% 
+      filter(TERRITORY1 == territory_i) %>% 
+      st_transform(., crs = 3460)
+    
     data_cyclones_i <- data_cyclones %>% 
-      filter(territory == territory_i)
+      filter(territory == territory_i) %>% 
+      group_by(saffir) %>% 
+      mutate(max_saffir = max(saffir)) %>% 
+      ungroup() %>% 
+      filter(max_saffir >= 1)
+    
+    if (nrow(data_cyclones_i) == 0) {
+      return(NULL)
+    }
+    
+    data_cyclones_i <- data_cyclones_i %>%  
+      mutate(time_range = case_when(time > as_date("1980-01-01") & time <= as_date("1989-12-31") ~ "1980 - 1989",
+                                    time > as_date("1990-01-01") & time <= as_date("1999-12-31") ~ "1990 - 1999",
+                                    time > as_date("2000-01-01") & time <= as_date("2009-12-31") ~ "2000 - 2009",
+                                    time > as_date("2010-01-01") & time <= as_date("2023-12-31") ~ "2010 - 2023"),
+             time_range = as.factor(time_range),
+             time_range = fct_expand(time_range, "1980 - 1989", "1990 - 1999", "2000 - 2009", "2010 - 2023"),
+             time_range = fct_relevel(time_range, c("1980 - 1989", "1990 - 1999", "2000 - 2009", "2010 - 2023")),
+             name = str_to_sentence(name)) %>% 
+      bind_cols(., color = sample(scico(nrow(.), begin = 0.3, end = 1, palette = "lajolla")))
     
     data_ts_lines_i <- left_join(data_cyclones_i, data_ts_lines) %>% 
       st_as_sf() %>% 
       st_transform(., crs = 3460) %>% 
       st_intersection(., data_eez_i %>% st_transform(crs = 3460))
-
-    ggplot() +
-      # EEZ borders
-      geom_sf(data = data_eez_i, color = "black", fill = "#89c4f4", alpha = 0.75) +
-      # Lands
+    
+    data_ts_points_i <- left_join(data_cyclones_i, data_ts_points) %>% 
+      st_as_sf() %>% 
+      st_transform(., crs = 3460) %>% 
+      st_intersection(., data_eez_i %>% st_transform(crs = 3460))
+    
+    plot_a <- ggplot() +
+      geom_sf(data = data_bathy_i %>% filter(depth == 0), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 200), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 1000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 2000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 3000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 4000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 5000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 6000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 7000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 8000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 9000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 10000), aes(fill = fill_color), color = NA) +
+      scale_fill_identity() +
+      geom_sf(data = data_eez_i, color = "black", alpha = 0.75) +
       geom_sf(data = data_land_i, fill = "#363737", col = "grey") +
-      # Cyclone path (those > n_cyclones)
-      geom_sf(data = data_ts_lines_i %>% filter(pos > n_cyclones), col = "darkgrey") +
-      # Cyclone path (those <= n_cyclones)
-      geom_sf(data = data_ts_lines_i %>% filter(pos <= n_cyclones), col = "#d91e18") +
+      geom_sf(data = data_ts_lines_i, aes(col = color), show.legend = FALSE) +
+      geom_sf(data = data_ts_points_i, aes(col = color), show.legend = FALSE, size = 0.75) +
+      geom_sf_label(data = data_ts_lines_i, aes(col = color, label = name), show.legend = FALSE,
+                    size = 1.75, label.size = 0, alpha = 0.85, label.r = unit(0.4, "lines")) +
+      scale_fill_identity() +
+      scale_color_identity() +
+      facet_wrap(~time_range, ncol = 2, drop = FALSE) +
       theme_minimal() +
       theme(axis.text = element_blank(),
             axis.ticks = element_blank(),
-            panel.grid = element_blank())
+            panel.grid = element_blank(),
+            strip.text.x = element_text(size = 12, family = font_choose_graph, face = "bold")) +
+      labs(x = NULL, y = NULL)
     
-  }else{
+    ggsave(filename = paste0("figs/territories_fig-3-a/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
+           plot = plot_a, height = 10, width = 6, dpi = 600)
     
+    plot_b <- ggplot(data = data_cyclones_i, aes(x = time, y = wind_speed)) +
+      geom_point(aes(fill = color), color = "white", shape = 21, size = 4, show.legend = FALSE) +
+      geom_label_repel(aes(label = name, color = color), fill = "white", alpha = 0.9,
+                       show.legend = FALSE, label.r = unit(0.4, "lines")) +
+      scale_y_continuous(breaks = c(0, 50 ,100, 150, 200, 250, 300), limits = c(0, 300)) +
+      coord_cartesian(ylim = c(14.25, 310)) +
+      scale_fill_identity() +
+      scale_color_identity() +
+      lims(x = c(ymd("1975-01-01"), ymd("2025-01-01"))) +
+      labs(x = "Year", y = bquote("Wind speed (km."~h^-1*")")) +
+      theme_graph() +
+      theme(panel.border = element_blank(),
+            panel.background = element_blank(),
+            panel.grid = element_blank(),
+            axis.ticks = element_line(linewidth = 0.5),
+            panel.grid.major.y = element_line(color = "black", linewidth = 0.5),
+            axis.text.y = element_text(vjust = -0.005),
+            plot.margin = margin(10, 4, 4, 4, "pt"))
+    
+    ggsave(filename = paste0("figs/territories_fig-3-b/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
+           plot = plot_b, height = 5, width = 8, dpi = 600)
+    
+    }else{
+  
     data_eez_i <- data_eez %>% 
       filter(TERRITORY1 == territory_i)
     
     data_land_i <- data_land %>% 
       filter(TERRITORY1 == territory_i)
     
-    data_cyclones_i <- data_cyclones %>% 
-      filter(territory == territory_i)
+    data_bathy_i <- data_bathy %>% 
+      filter(TERRITORY1 == territory_i)
     
+    data_cyclones_i <- data_cyclones %>% 
+      filter(territory == territory_i) %>% 
+      group_by(saffir) %>% 
+      mutate(max_saffir = max(saffir)) %>% 
+      ungroup() %>% 
+      filter(max_saffir >= 1)
+    
+    if (nrow(data_cyclones_i) == 0) {
+      return(NULL)
+    }
+    
+    data_cyclones_i <- data_cyclones_i %>%  
+      mutate(time_range = case_when(time > as_date("1980-01-01") & time <= as_date("1989-12-31") ~ "1980 - 1989",
+                                    time > as_date("1990-01-01") & time <= as_date("1999-12-31") ~ "1990 - 1999",
+                                    time > as_date("2000-01-01") & time <= as_date("2009-12-31") ~ "2000 - 2009",
+                                    time > as_date("2010-01-01") & time <= as_date("2023-12-31") ~ "2010 - 2023"),
+             time_range = as.factor(time_range),
+             time_range = fct_expand(time_range, "1980 - 1989", "1990 - 1999", "2000 - 2009", "2010 - 2023"),
+             time_range = fct_relevel(time_range, c("1980 - 1989", "1990 - 1999", "2000 - 2009", "2010 - 2023")),
+             name = str_to_sentence(name)) %>% 
+      bind_cols(., color = sample(scico(nrow(.), begin = 0.3, end = 1, palette = "lajolla")))
+             
     data_ts_lines_i <- left_join(data_cyclones_i, data_ts_lines) %>% 
       st_as_sf() %>% 
       st_intersection(., data_eez_i %>% st_transform(crs = 4326))
     
-    ggplot() +
-      # EEZ borders
-      geom_sf(data = data_eez_i, color = "black", fill = "#89c4f4", alpha = 0.75) +
-      # Lands
+    data_ts_points_i <- left_join(data_cyclones_i, data_ts_points) %>% 
+      st_as_sf() %>% 
+      st_intersection(., data_eez_i %>% st_transform(crs = 4326))
+
+    plot_a <- ggplot() +
+      geom_sf(data = data_bathy_i %>% filter(depth == 0), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 200), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 1000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 2000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 3000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 4000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 5000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 6000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 7000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 8000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 9000), aes(fill = fill_color), color = NA) +
+      geom_sf(data = data_bathy_i %>% filter(depth == 10000), aes(fill = fill_color), color = NA) +
+      scale_fill_identity() +
+      geom_sf(data = data_eez_i, color = "black", alpha = 0.75) +
       geom_sf(data = data_land_i, fill = "#363737", col = "grey") +
-      # Cyclone path (those > n_cyclones)
-      geom_sf(data = data_ts_lines_i %>% filter(pos > n_cyclones), col = "darkgrey") +
-      # Cyclone path (those <= n_cyclones)
-      geom_sf(data = data_ts_lines_i %>% filter(pos <= n_cyclones), col = "#d91e18") +
+      geom_sf(data = data_ts_lines_i, aes(col = color), show.legend = FALSE) +
+      geom_sf(data = data_ts_points_i, aes(col = color), show.legend = FALSE, size = 0.75) +
+      geom_sf_label(data = data_ts_lines_i, aes(col = color, label = name), show.legend = FALSE,
+                   size = 1.75, label.size = 0, alpha = 0.85, label.r = unit(0.3, "lines")) +
+      scale_fill_identity() +
+      scale_color_identity() +
+      facet_wrap(~time_range, ncol = 2, drop = FALSE) +
       theme_minimal() +
       theme(axis.text = element_blank(),
             axis.ticks = element_blank(),
-            panel.grid = element_blank())
+            panel.grid = element_blank(),
+            strip.text.x = element_text(size = 12, family = font_choose_graph, face = "bold")) +
+      labs(x = NULL, y = NULL)
     
-  }
-  
-  ggsave(filename = paste0("figs/territories_fig-3-b/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"), dpi = 600)
-  
+    ggsave(filename = paste0("figs/territories_fig-3-a/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
+           plot = plot_a, height = 10, width = 6, dpi = 600)
+    
+    plot_b <- ggplot(data = data_cyclones_i, aes(x = time, y = wind_speed)) +
+      geom_point(aes(fill = color), color = "white", shape = 21, size = 4, show.legend = FALSE) +
+      geom_label_repel(aes(label = name, color = color), fill = "white", alpha = 0.9,
+                       show.legend = FALSE, label.r = unit(0.4, "lines")) +
+      scale_y_continuous(breaks = c(0, 50 ,100, 150, 200, 250, 300), limits = c(0, 300)) +
+      coord_cartesian(ylim = c(14.25, 310)) +
+      scale_fill_identity() +
+      scale_color_identity() +
+      lims(x = c(ymd("1975-01-01"), ymd("2025-01-01"))) +
+      labs(x = "Year", y = bquote("Wind speed (km."~h^-1*")")) +
+      theme_graph() +
+      theme(panel.border = element_blank(),
+            panel.background = element_blank(),
+            panel.grid = element_blank(),
+            axis.ticks = element_line(linewidth = 0.5),
+            panel.grid.major.y = element_line(color = "black", linewidth = 0.5),
+            axis.text.y = element_text(vjust = -0.005),
+            plot.margin = margin(10, 4, 4, 4, "pt"))
+    
+    ggsave(filename = paste0("figs/territories_fig-3-b/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
+           plot = plot_b, height = 5, width = 8, dpi = 600)
+
+    }
+    
 }
 
 # 6.2 Map over the function --
 
-map(unique(data_cyclones$territory), ~map_cyclone_map(territory_i = .))
+map(unique(data_cyclones$territory), ~map_cyclone(territory_i = .))
