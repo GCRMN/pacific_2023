0157d63a9d6a5239be7010b95fc5a3225204a536
JWicquart <jeremywicquart@gmail.com>
2023-07-18 10:28:41 +0200

    include mapping functions directly within scripts

--- code/02_extract_sst-time-series.R
+++ code/02_extract_sst-time-series.R
@@ -10,15 +10,11 @@ library(furrr)
 
 plan(multisession, workers = 6) # Set parallelization with 6 cores
 
-# 2. Required functions ----
-
-source("code/function/ncdf_extract.R") # NetCDF SST extraction
-
-# 3. List of NetCDF4 files ----
+# 2. List of NetCDF4 files ----
 
 ncdf_files <- list.files(path = "../disturbance/data/011_sst_raw/", pattern = "\\.nc$", full.names = TRUE)
 
-# 4. Check if files are missing ----
+# 3. Check if files are missing ----
 
 real_files_list <- str_remove_all(str_split_fixed(ncdf_files, "_", n = 5)[,5], "\\.nc")
 
@@ -28,11 +24,30 @@ setdiff(theoric_files_list, real_files_list)
 
 rm(theoric_files_list, real_files_list)
 
-# 5. File of site coordinates ----
+# 4. File of EEZ ----
 
 data_reef <- st_read("data/03_reefs-area_wri/clean/pacific_reef.shp")
 
-# 6. Extract the data for each NCDF file ----
+# 5. Create function to extract SST ----
+
+ncdf_extract <- function(ncdf_i){
+  
+  ncdf <- rast(ncdf_i)
+  
+  my_summary <- function(x) c(mean = mean(x, na.rm = TRUE),
+                              min = min(x, na.rm = TRUE),
+                              max = max(x, na.rm = TRUE),
+                              sd = sd(x, na.rm = TRUE))
+  
+  sst_i <- terra::extract(x = ncdf, y = data_reef, fun = my_summary) %>% 
+    select(-starts_with("sea")) %>% 
+    mutate(date = unique(time(ncdf)))
+  
+  return(sst_i)
+  
+}
+
+# 6. Map over the function ----
 
 data_sst <- future_map_dfr(ncdf_files, ~ncdf_extract(.)) %>% 
   rename(sst = analysed_sst) %>% 
--- code/03_geographic-informations.R
+++ code/03_geographic-informations.R
@@ -4,11 +4,7 @@ library(tidyverse)
 library(sf)
 sf_use_s2(FALSE)
 
-# 2. Source functions ----
-
-source("code/function/export_geoinf.R")
-
-# 3. Maritime area ----
+# 2. Maritime area ----
 
 load("data/01_background-shp/03_eez/data_eez.RData")
 
@@ -20,7 +16,7 @@ data_maritime_area <- data_eez %>%
   ungroup() %>% 
   mutate(AREA_KM2 = format(AREA_KM2, big.mark = ","))
 
-# 4. Reef area ----
+# 3. Reef area ----
 
 data_reefs <- st_read("data/03_reefs-area_wri/reef_500_poly.shp") %>% 
   st_transform(crs = 4326) %>% 
@@ -45,7 +41,7 @@ data_reef_area <- st_intersection(data_eez, data_reefs) %>%
          area = round(area, 0),
          area = format(area, big.mark = ","))
 
-# 5. Human population ----
+# 4. Human population ----
 
 data_population <- read.csv("data/02_geo-inf/01_human-pop.csv") %>% 
   rename(population = sum) %>% 
@@ -55,29 +51,35 @@ data_population <- read.csv("data/02_geo-inf/01_human-pop.csv") %>%
   mutate(population = round(population, 0),
          population = format(population, big.mark = ",", scientific = FALSE))
 
-# 6. Export ---- 
+# 5. Create a function to produce geographic information output ----
+
+export_geoinf <- function(territory_i){
+  
+  data_maritime_area_i <- data_maritime_area %>% 
+    filter(TERRITORY1 == territory_i) %>% 
+    select(AREA_KM2) %>% 
+    pull()
+  
+  data_reef_area_i <- data_reef_area %>% 
+    filter(TERRITORY1 == territory_i) %>% 
+    select(area) %>% 
+    pull()
+  
+  data_population_i <- data_population %>% 
+    filter(TERRITORY1 == territory_i) %>% 
+    select(population) %>% 
+    pull()
+  
+  writeLines(c("\\begin{tabular}{>{\\bfseries}>{\\color{color1}}rl}",
+               paste0("Maritime area & ", data_maritime_area_i, " km\\textsuperscript{2} \\\\"),
+               "Land area & 12,000 km\\textsuperscript{2} \\\\",
+               paste0("Reef area & ", data_reef_area_i, " km\\textsuperscript{2} \\\\"),
+               paste0("Population (2020) & ", data_population_i, " \\\\"),
+               "\\end{tabular}"),
+             paste0("figs/03_geo-inf/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".tex"))
+  
+}
+
+# 6. Map over the function ----
 
 map(unique(data_eez$TERRITORY1), ~export_geoinf(territory_i = .))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-library(tidyverse)
-A <- read_csv("data/reefsurface_by_site.csv") %>% 
-  select(GEONAME, sum)
-
-
-
-
-
-
