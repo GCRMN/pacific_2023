a19e04e3e6d8e99c14b2baae981c242c0d7e7159
JWicquart <jeremywicquart@gmail.com>
2023-07-19 09:48:31 +0200

    add global mean SST ocean change

--- code/03_pacific_sst.R
+++ code/03_pacific_sst.R
@@ -21,11 +21,18 @@ data_warming <- read.csv2("figs/sst_indicators.csv") %>%
 # 4. Make the plot ----
 
 ggplot(data = data_warming, aes(x = sst_increase, y = fct_reorder(TERRITORY1, sst_increase))) +
+  # Global mean SST change
+  # The number 0.88°C is coming from Technical summary IPCC, 2021 (TS.2.4, The Ocean, page 74)
+  geom_vline(xintercept = (0.88/(2020-1900))*(2022-1980), color = "#34495e", linetype = "dashed") + 
   geom_segment(aes(xend = 0, yend = fct_reorder(TERRITORY1, sst_increase))) +
   geom_point(aes(fill = color), shape = 21, size = 3) +
   scale_fill_identity() +
   scale_color_identity() +
   geom_vline(xintercept = 0) +
+  annotate("curve", x = (0.88/(2020-1900))*(2022-1980), xend = 0.5, y = 3, yend = 2, curvature = 0.25,
+           arrow = arrow(length = unit(0.015, "npc"), ends = "first"), color = "#34495e") +
+  annotate("text", x = 0.52, y = 2.1, label = "Global mean SST ocean change",
+           family = font_choose_graph, hjust = 0, size = 3, color = "#34495e") +
   labs(x = "Change in SST (°C) between 1980 and 2022", y = NULL) +
   lims(x = c(-0.45, 1.25))
 
