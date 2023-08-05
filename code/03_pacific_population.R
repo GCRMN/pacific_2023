700e69e1dd88320ce56cffad5b022fbc0fdfae55
JWicquart <jeremywicquart@gmail.com>
2023-08-05 15:02:39 +0200

    replace geom point by geom area

--- code/03_pacific_population.R
+++ code/03_pacific_population.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(sf)
+library(scico)
 sf_use_s2(FALSE)
 
 # 2. Source functions ----
@@ -14,18 +15,31 @@ source("code/function/theme_graph.R")
 data_population <- read.csv("data/02_geo-inf/01_human-pop.csv") %>% 
   rename(population = sum, territory = TERRITORY1, year = date) %>% 
   mutate(year = year(year)) %>% 
-  group_by(year) %>% 
+  mutate(population = population*1e-06, # Convert to million
+         territory_type = if_else(territory %in% c("Papua New Guinea", "Fiji", "Hawaii"),
+                                  territory, 
+                                  "Other territories")) %>% 
+  group_by(territory_type, year) %>% 
   summarise(population = sum(population)) %>% 
-  ungroup() %>% 
-  mutate(population = population*1e-06) # Convert to million
+  ungroup()
 
 # 4. Make the plot ----
 
-ggplot(data = data_population, aes(x = year, y = population)) +
-  geom_line() +
-  geom_point(size = 4, shape = 21, fill = "#446cb3", color = "white") +
+ggplot(data = data_population, aes(x = year, y = population, fill = territory_type)) +
+  geom_area(show.legend = FALSE) +
+  scale_fill_manual(values = scico(5, begin = 0.3, end = 1, palette = "lajolla")) +
   labs(x = "Year", y = "Number of inhabitants (millions)") +
-  theme_graph()
+  annotate(geom = "text", label = "Papua New Guinea", x = 2003, y = 3, 
+           family = font_choose_graph, color = "white", hjust = 0) +
+  annotate(geom = "text", label = "Other territories", x = 2003, y = 7, 
+           family = font_choose_graph, color = "white", hjust = 0) +
+  annotate(geom = "text", label = "Hawaii", x = 2003, y = 8.5, 
+           family = font_choose_graph, color = "white", hjust = 0) +
+  annotate(geom = "text", label = "Fiji", x = 2003, y = 9.4, 
+           family = font_choose_graph, color = "white", hjust = 0) +
+  theme_graph() + 
+  lims(y = c(0, 15)) +
+  guides(fill = guide_legend(reverse=TRUE))
 
 # 5. Export the plot ----
 
