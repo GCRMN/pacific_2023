472130776aa98c0bbee95a1378ca0149818f48d3
JWicquart <jeremywicquart@gmail.com>
2023-12-04 16:15:21 +0100

    download coraltemp netcdf4 files using R

---
+++ code/01_download_coraltemp.R
@@ -0,0 +1,28 @@
+# 1. Required packages ----
+
+library(tidyverse)
+
+# 2. List files to download ----
+
+list_url <- data.frame(date = seq(from = ymd("1985-01-01"), to = ymd("2023-11-30"), by = "1 day")) %>% 
+  mutate(year = year(date),
+         date = str_remove_all(date, "-"),
+         url = paste0("https://www.star.nesdis.noaa.gov/pub/sod/mecb/crw/data/5km/v3.1_op/nc/v1.0/daily/sst/",
+                      year,
+                      "/coraltemp_v3.1_",
+                      date,
+                      ".nc"),
+         filename = str_split_fixed(url, "/", Inf)[,16])
+
+# 3. Download NetCDF4 files ----
+
+for(i in 1:nrow(list_url)){
+  
+  if(file.exists(paste0("data/06_sst/", list_url[i, "filename"])) == FALSE){
+    
+    download.file(url = list_url[i, "url"],
+                  destfile = paste0("data/06_sst/", list_url[i, "filename"]))
+    
+  }
+  
+}
