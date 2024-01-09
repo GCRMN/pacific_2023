7a282580dcae1c21ed824e321337826169116c8c
JWicquart <jeremywicquart@gmail.com>
2024-01-09 17:44:24 +0100

    add bathymetry on background map

--- code/03_pacific_spatio-temporal.R
+++ code/03_pacific_spatio-temporal.R
@@ -12,17 +12,19 @@ source("code/function/graphical_par.R")
 source("code/function/theme_map.R")
 source("code/function/theme_graph.R")
 
-# 3. Map of spatio-temporal distribution of monitoring sites ----
+# 3. Map of spatio-temporal distribution ----
 
-# 3.1 Define the CRS --
+## 3.1 Change the CRS ----
+
+### 3.1.1 Define the CRS ----
 
 crs_selected <- "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=160 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
 
-# 3.2 Define the offset --
+### 3.1.2 Define the offset ----
 
 correction_offset <- 180 - 160 # Here 160 is the same value than +lon_0 from crs_selected
 
-# 3.3 Define a long and slim polygon that overlaps the meridian line --
+### 3.1.3 Define a long and slim polygon that overlaps the meridian line ----
 
 correction_polygon <- st_polygon(x = list(rbind(c(-0.0001 - correction_offset, 90),
                                                 c(0 - correction_offset, 90),
@@ -32,14 +34,14 @@ correction_polygon <- st_polygon(x = list(rbind(c(-0.0001 - correction_offset, 9
   st_sfc() %>%
   st_set_crs(4326)
 
-# 3.4. Load background maps --
+## 3.2 Load background maps ----
 
 data_map <- read_sf("data/01_background-shp/01_ne/ne_10m_land/ne_10m_land.shp") %>% 
   st_transform(crs = 4326) %>% 
   st_difference(correction_polygon) %>% 
   st_transform(crs_selected)
 
-# 3.5 Economic Exclusive Zones --
+## 3.3 Economic Exclusive Zones ----
 
 load("data/01_background-shp/03_eez/data_eez.RData")
 
@@ -52,12 +54,23 @@ data_eez_disputed <- read_sf("data/01_background-shp/03_eez/World_EEZ_v12_202310
   st_difference(correction_polygon) %>% 
   st_transform(crs_selected)
 
-# 3.6 Country boundaries --
+data_eez <- bind_rows(data_eez, data_eez_disputed)
+
+## 3.4 Country boundaries ----
 
 data_countries <- read_sf("data/01_background-shp/01_ne/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp") %>% 
   st_transform(crs_selected)
 
-# 3.7 Create the tropics --
+## 3.5 Bathymetry ----
+
+load("data/01_background-shp/01_ne/ne_10m_bathymetry_all.RData")
+
+data_bathy <- data_bathy %>% 
+  st_transform(crs = 4326) %>% 
+  st_difference(correction_polygon) %>% 
+  st_transform(crs_selected)
+
+## 3.6 Create the tropics ----
 
 data_tropics <- tibble(long = c(-180, 180, -180, 180, -180, 180), 
                        lat = c(0, 0, 23.43656, 23.43656, -23.43656, -23.43656), 
@@ -76,9 +89,9 @@ data_tropics_no_eez$var <- "true"
 
 data_tropics_no_eez <- st_difference(data_tropics, st_union(st_geometry(data_tropics_no_eez)))
 
-# 3.8 Create text annotation --
+## 3.7 Create text annotation ----
 
-# 3.8.1 Tropics --
+### 3.7.1 Tropics ----
 
 data_text_tropics <- tibble(long = c(-105, -104, -119, -128),
                             lat = c(-21.43, -25.43, 2, 25.43),
@@ -86,7 +99,7 @@ data_text_tropics <- tibble(long = c(-105, -104, -119, -128),
   st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
   st_transform(crs_selected)
 
-# 3.8.2 Pacific Ocean --
+### 3.7.2 Pacific Ocean ----
 
 data_text_pacific <- tibble(long = c(-130),
                             lat = c(13),
@@ -94,7 +107,7 @@ data_text_pacific <- tibble(long = c(-130),
   st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
   st_transform(crs_selected)
 
-# 3.8.3 Australia --
+### 3.7.3 Australia ----
 
 data_text_australia <- tibble(long = c(140),
                               lat = c(-25),
@@ -102,7 +115,19 @@ data_text_australia <- tibble(long = c(140),
   st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
   st_transform(crs_selected)
 
-# 3.9 Select benthic data --
+## 3.8 Bbox for alpha on bathymetry ---
+
+data_alpha <- tibble(lat = c(-4000000, 4000000),
+                     lon = c(-3500000, 11000000)) %>% 
+  st_as_sf(coords = c("lon", "lat"), crs = crs_selected) %>% 
+  st_bbox() %>% 
+  st_as_sfc()
+
+data_alpha <- data_eez %>% 
+  summarise(geometry = st_union(geometry)) %>% 
+  st_difference(data_alpha, .)
+
+## 3.9 Select benthic data ----
 
 load("data/04_data-benthic.RData")
 
@@ -119,15 +144,27 @@ data_benthic <- data_benthic %>%
   st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326) %>% 
   st_transform(crs = crs_selected)
 
-# 3.10 Make the plot --
+## 3.10 Make the map ----
 
-plot_a <- ggplot() +
+ggplot() +
+  geom_sf(data = data_bathy %>% filter(depth == 0), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 200), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 1000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 2000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 3000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 4000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 5000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 6000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 7000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 8000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 9000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  geom_sf(data = data_bathy %>% filter(depth == 10000), aes(fill = fill_color), color = NA, alpha = 0.2) +
+  scale_fill_identity() +
+  geom_sf(data = data_alpha, fill = "white", alpha = 0.5) +
   # Tropics
-  #geom_sf(data = data_tropics, linetype = "dashed", color = "#363737", linewidth = 0.25) +
   geom_sf(data = data_tropics_no_eez, linetype = "dashed", color = "#363737", linewidth = 0.25) +
   # EEZ
-  geom_sf(data = data_eez_disputed, color = "#95a5a6", fill = "#e4e9ed") +
-  geom_sf(data = data_eez, color = "#5c97bf", fill = "#bbd9eb", alpha = 0.75) +
+  geom_sf(data = data_eez, color = "#363737", fill = "#e4e9ed", alpha = 0.2) +
   # Background map
   geom_sf(data = data_map, fill = "#363737", col = "grey") +
   # Country boundaries
@@ -143,49 +180,67 @@ plot_a <- ggplot() +
   geom_sf_text(data = data_text_tropics, aes(label = text), hjust = 1,
                color = "#363737", size = 2.5, family = font_choose_map, fontface = "italic") +
   geom_sf_text(data = data_text_pacific, aes(label = text), 
-               color = "#5c97bf", fontface = "italic", size = 3, family = font_choose_map) +
+               color = "#1e517b", fontface = "italic", size = 3, family = font_choose_map) +
   # Graphical aspects
   coord_sf(ylim = c(-4000000, 4000000), xlim = c(-3500000, 11000000), expand = FALSE) +
   scale_x_continuous(breaks = c(180, 160, 140, -160, -140, -120)) +
   theme(text = element_text(family = font_choose_map),
         panel.background = element_rect(fill = "#ebf5fd"),
+        legend.position = "top",
+        legend.direction = "horizontal",
+        legend.key = element_blank(),
         panel.grid = element_blank(),
         panel.border = element_rect(fill = NA, color = "black", linewidth = 1),
         axis.title = element_blank(),
-        axis.text.x.top = element_text(),
-        legend.position = "top",
-        legend.direction = "horizontal",
-        legend.key = element_blank()) +
+        axis.text.x.top = element_text()) +
   guides(colour = guide_legend(title.position = "top", title.hjust = 0.5, override.aes = list(size = 4)))
 
-# 3.11 Save the plot --
+## 3.11 Save the plot ----
 
-ggsave(filename = "figs/01_pacific-map-sites.png", width = 8, height = 5, dpi = 600)
+ggsave(filename = "figs/01_part-1/fig-8.png", width = 8, height = 5, dpi = 600)
 
 # 4. Plot of percentage of sites per interval_class ----
 
-plot_a <- data_benthic %>% 
+# 4.1 Make the plot ----
+
+data_benthic %>% 
   st_drop_geometry() %>% 
   group_by(interval_class) %>% 
   count() %>% 
   ungroup() %>% 
   mutate(percent = n*100/sum(n)) %>% 
-  ggplot(data = ., aes(x = interval_class, y = percent, fill = interval_class)) +
+  ggplot(data = ., aes(x = reorder(interval_class, desc(interval_class)),
+                       y = percent, fill = interval_class)) +
     geom_bar(stat = "identity", color = NA, show.legend = FALSE, width = 0.65) +
     scale_fill_manual(values = palette_5cols,
                       labels = c("1 year", "2-5 years", "6-10 years", "11-15 years", ">15 years"), 
                       drop = FALSE, name = "Number of years with data") +
-    labs(x = "Duration", y = "Sites (%)", title = "A") +
-    lims(y = c(0, 100)) +
-    theme_graph() +
-    theme(plot.title = element_text(size = 15),
-          axis.text.x = element_text(size = 7))
+    labs(x = NULL, y = "Sites (%)") +
+    coord_flip(clip = "off") +
+    theme(axis.ticks = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(),
+          #panel.grid.minor.x = element_blank(),
+          #panel.grid.major.y = element_line(),
+          axis.title.x = element_text(size = 12, margin = margin(t = 7, r = 0, b = 0, l = 0)),
+          axis.line.x = element_line(linewidth = 0.4),
+          axis.ticks.x = element_line(linewidth = 0.4),
+          axis.text.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
+          axis.text.x = element_text(margin = margin(t = 7, r = 0, b = 0, l = 0)),
+          plot.margin = margin(0.5, 0.5, 0.5, 0.5, "cm")) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, 100))
+
+## 4.2 Save the plot ----
+
+ggsave(filename = "figs/01_part-1/fig-9.png", width = 6, height = 4, dpi = 600)
 
 # 5. Plot of number of surveys per year ----
 
+# 5.1 Make the plot ----
+
 load("data/04_data-benthic.RData")
 
-plot_b <- data_benthic %>% 
+data_benthic %>% 
   select(territory, decimalLatitude, decimalLongitude, eventDate, year) %>% 
   st_drop_geometry() %>% 
   distinct() %>% 
@@ -196,22 +251,12 @@ plot_b <- data_benthic %>%
   mutate(percent = n*100/sum(n)) %>% 
   ggplot(data = ., aes(x = year, y = percent)) +
     geom_bar(stat = "identity", show.legend = FALSE, width = 0.5, color = "#C9504B", fill = "#C9504B") +
-    labs(x = "Year", y = "Surveys (%)", title = "B") +
+    labs(x = "Year", y = "Surveys (%)") +
     lims(x = c(1970, 2024)) +
     theme_graph() +
     theme(plot.title = element_text(size = 15),
           axis.text.x = element_text(size = 7))
 
-# 6. Combine plots ----
+## 5.2 Save the plot ----
 
-# 6.1 Horizontal --
-
-plot_a + plot_b + plot_layout(ncol = 2)
-
-ggsave(filename = "figs/01_pacific_surveys_lds.png", width = 10, height = 4, dpi = 600)
-
-# 6.2 Vertical --
-
-plot_a + plot_b + plot_layout(ncol = 1)
-
-ggsave(filename = "figs/01_pacific_surveys_prt.png", width = 5, height = 8, dpi = 600)
+ggsave(filename = "figs/01_part-1/fig-10.png", width = 5, height = 3.5, dpi = 600)
