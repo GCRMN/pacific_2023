bfe0ed5aa8296c4a9d8f960c3a492efac58adebb
JWicquart <jeremywicquart@gmail.com>
2024-03-01 13:28:17 +0100

    include lag values for sst mean, sst max, and dhw max

--- code/02_model_data-preparation.R
+++ code/02_model_data-preparation.R
@@ -83,15 +83,24 @@ data_predictors <- read.csv("data/10_predictors/pred_sst_kurtosis.csv") %>%
   left_join(data_predictors, .)
 
 data_predictors <- read.csv("data/10_predictors/pred_sst_max.csv") %>% 
+  arrange(site_id, type, year) %>% 
+  group_by(site_id, type) %>% 
+  mutate(pred_sst_max_y1 = lag(pred_sst_max, n = 1)) %>% 
   left_join(data_predictors, .)
 
 data_predictors <- read.csv("data/10_predictors/pred_sst_mean.csv") %>% 
+  arrange(site_id, type, year) %>% 
+  group_by(site_id, type) %>% 
+  mutate(pred_sst_mean_y1 = lag(pred_sst_mean, n = 1)) %>% 
   left_join(data_predictors, .)
 
 data_predictors <- read.csv("data/10_predictors/pred_sst_min.csv") %>% 
   left_join(data_predictors, .)
 
 data_predictors <- read.csv("data/10_predictors/pred_dhw_max.csv") %>% 
+  arrange(site_id, type, year) %>% 
+  group_by(site_id, type) %>% 
+  mutate(pred_dhw_max_y1 = lag(pred_dhw_max, n = 1)) %>% 
   left_join(data_predictors, .)
 
 data_predictors <- read.csv("data/10_predictors/pred_chla_mean.csv") %>% 
@@ -107,13 +116,18 @@ data_predictors <- left_join(data_predictors, pred_human_pop)
 data_predictors <- data_predictors %>% 
   # Change unit for SST (°C)
   mutate(across(c(pred_sst_sd, pred_sst_max, 
-                  pred_sst_mean, pred_sst_min), ~.x/100)) %>%
+                  pred_sst_mean, pred_sst_min,
+                  pred_sst_max_y1, pred_sst_mean_y1), ~.x/100)) %>%
   # Round to 3 digits
   mutate(across(c(pred_elevation, pred_reefextent, pred_land,
-                  pred_enso, pred_sst_sd, pred_sst_skewness,
+                  pred_enso, pred_chla_mean, pred_chla_sd),
+                ~ round(.x, digits = 3))) %>% 
+  # Round to 2 digits
+  mutate(across(c(pred_sst_sd, pred_sst_skewness,
                   pred_sst_kurtosis, pred_sst_max, pred_sst_mean,
-                  pred_sst_min, pred_dhw_max,
-                  pred_chla_mean, pred_chla_sd), ~ round(.x, digits = 3)))
+                  pred_sst_min, pred_dhw_max, pred_dhw_max_y1,
+                  pred_sst_max_y1, pred_sst_mean_y1),
+                ~ round(.x, digits = 2)))
 
 # 6. Split predictors in observed and to predict tibbles ----
 
@@ -130,50 +144,44 @@ data_predictors_pred <- data_predictors %>%
   select(-type, -site_id) %>% 
   mutate(datasetID = NA,
          month = NA,
+         day = NA,
          verbatimDepth = NA,
          parentEventID = NA,
          eventID = NA)
 
 save(data_predictors_pred, file = "data/11_model-data/data_predictors_pred.RData")
 
-# 7. Transform benthic data ----
+# 7. Summarize data and add predictors ----
 
-## 7.1 Modify NCRMP data (from semi-quantitative to quantitative) by averaging at the scale of a transect ----
-
-data_benthic_ncrmp <- data_benthic %>% 
-  filter(datasetID %in% c("0011", "0012", "0013", "0014")) %>% 
-  group_by(datasetID, higherGeography, country, territory, locality, habitat, parentEventID,
-           decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category, subcategory) %>% 
-  summarise(measurementValue = sum(measurementValue)) %>% 
-  ungroup() %>% 
-  group_by(datasetID, higherGeography, country, territory, locality, habitat, parentEventID,
-           decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, category, subcategory) %>% 
-  summarise(measurementValue = mean(measurementValue)) %>% 
-  ungroup() %>% 
-  filter(measurementValue <= 100)
-
-## 7.2 Summarize data and add predictors ----
+## 7.1 Transform the data ----
 
 data_benthic <- data_benthic %>% 
-  filter(!(datasetID %in% c("0011", "0012", "0013", "0014", "0039", "0043", "0051", "0052", "0053"))) %>% 
-  bind_rows(., data_benthic_ncrmp) %>% 
+  # 1. Sum of benthic cover per sampling unit (site, transect, quadrat) and category
   mutate(category = case_when(subcategory == "Macroalgae" ~ "Macroalgae",
                               subcategory == "Turf algae" ~ "Turf algae",
                               subcategory == "Coralline algae" ~ "Coralline algae",
                               TRUE ~ category)) %>% 
   filter(category %in% c("Hard coral", "Macroalgae", "Turf algae", "Coralline algae")) %>% 
-  # Sum of benthic cover per sampling unit and category
   group_by(datasetID, higherGeography, country, territory, locality, habitat, parentEventID,
            decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, eventID, category) %>% 
   summarise(measurementValue = sum(measurementValue)) %>% 
   ungroup() %>% 
-  # Remove useless variables
-  select(-higherGeography, -country, -locality, -habitat, -eventDate, -day) %>% 
-  # Convert to factors
+  # 2. Summarise data at the transect level (i.e. mean of photo-quadrats)
+  # This avoid getting semi-quantitative data (e.g. when there is only 10 points per photo-quadrat)
+  # This is the case for datasets "0011", "0012", "0013", "0014", and "0043" at least
+  group_by(datasetID, higherGeography, country, territory, locality, habitat, parentEventID,
+           decimalLatitude, decimalLongitude, verbatimDepth, year, month, day, eventDate, category) %>% 
+  summarise(measurementValue = mean(measurementValue)) %>% 
+  ungroup() %>% 
+  # 3. Remove values grater than 100 (unlikely but included to avoid any issues later)
+  filter(measurementValue <= 100) %>% 
+  # 4. Remove useless variables
+  select(-higherGeography, -country, -locality, -habitat, -eventDate) %>% 
+  # 5. Convert to factors
   mutate_if(is.character, factor) %>% 
-  # Add predictors
+  # 6. Add predictors
   left_join(., data_predictors_obs)
 
-## 7.3 Export data ----
+## 7.2 Export the data ----
 
 save(data_benthic, file = "data/11_model-data/data_benthic_prepared.RData")
