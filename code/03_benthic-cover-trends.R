2aaade70658636942003991d64f3bf642f4054fb
JWicquart <jeremywicquart@gmail.com>
2024-03-06 17:58:39 +0100

    misc updates on model outputs dataviz

--- code/03_benthic-cover-trends.R
+++ code/03_benthic-cover-trends.R
@@ -13,6 +13,8 @@ theme_set(theme_graph())
 
 # 3. Load and combine model results ----
 
+## 3.1 Load data ----
+
 load("data/12_model-output/model_results_hard-coral.RData")
 model_results_coral <- model_results
 
@@ -25,14 +27,37 @@ model_results_turf <- model_results
 load("data/12_model-output/model_results_coralline-algae.RData")
 model_results_cca <- model_results
 
+## 3.2 Combine data ----
+
 model_results <- lst(model_results_coral, model_results_macroalgae,
                      model_results_turf, model_results_cca) %>% 
   map_df(., ~ as.data.frame(map(.x, ~ unname(nest(.))))) %>% 
   map(., bind_rows)
 
-save("data/12_model-output/model_results_all.RData")
+## 3.3 Export combined data ----
+
+save(model_results, file = "data/12_model-output/model_results_all.RData")
 
 rm(model_results_coral, model_results_macroalgae, model_results_turf, model_results_cca)
+
+## 3.4 Add the color, text titles and factor levels ----
+
+model_results <- model_results %>% 
+  map(., ~ .x %>% mutate(color = case_when(category == "Hard coral" ~ palette_second[2],
+                                           category == "Coralline algae" ~ palette_second[3],
+                                           category == "Macroalgae" ~ palette_second[4],
+                                           category == "Turf algae" ~ palette_second[5]),
+                         text_title = case_when(category == "Hard coral" ~ 
+                                                  glue("**A.**<span style='color:{color}'> {category}</span>"),
+                                                category == "Coralline algae" ~ 
+                                                  glue("**B.**<span style='color:{color}'> {category}</span>"),
+                                                category == "Macroalgae" ~ 
+                                                  glue("**C.**<span style='color:{color}'> {category}</span>"),
+                                                category == "Turf algae" ~ 
+                                                  glue("**D.**<span style='color:{color}'> {category}</span>")),
+      category = as.factor(category),
+      category = fct_expand(category, "Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+      category = fct_relevel(category, "Hard coral", "Coralline algae", "Macroalgae", "Turf algae")))
 
 # 4. Model characteristics ----
 
@@ -64,6 +89,12 @@ model_results$model_time %>%
 
 ## 4.2 Hyperparameters ----
 
+
+
+
+
+
+
 # 5. Model performance ----
 
 ## 5.1 RMSE and R² ----
@@ -84,13 +115,14 @@ A <- model_results$result_pred_obs %>%
   ungroup() %>% 
   select(-res, -tot)
 
-ggplot(data = A, aes(x = territory, y = rmse)) +
+ggplot(data = A, aes(x = reorder(territory, desc(territory)), y = rmse)) +
   geom_hline(yintercept = mean(unique(A$rmse_global))) +
   geom_point() +
   coord_flip() +
+  facet_wrap(~category, nrow = 1) +
   labs(x = NULL, y = "RMSE")
 
-ggplot(data = A, aes(x = territory, y = rsq)) +
+ggplot(data = A, aes(x = reorder(territory, desc(territory)), y = rsq)) +
   geom_hline(yintercept = mean(unique(A$rsq_global))) +
   geom_point() +
   coord_flip() +
@@ -98,169 +130,134 @@ ggplot(data = A, aes(x = territory, y = rsq)) +
 
 ## 5.2 Predicted vs observed ----
 
+model_results$result_pred_obs %>% 
+  ggplot(data = ., aes(x = y, y = yhat, color= color)) +
+  geom_point(alpha = 0.1) +
+  geom_abline(slope = 1) +
+  scale_color_identity() +
+  facet_wrap(~text_title, scales = "free") +
+  labs(x = "Observed value (y)", y = "Predicted value (ŷ)") +
+  theme(strip.text = element_markdown(hjust = 0),
+        strip.background = element_blank())
+
+ggsave("figs/05_additional/02_model/02_pred-vs-obs_all.png", dpi = 600)
+
 ## 5.3 Distribution of residuals ----
+
+model_results$result_pred_obs %>% 
+  mutate(residual = yhat - y) %>% 
+  ggplot(data = ., aes(x = residual, fill = color)) + 
+  geom_histogram(aes(y = after_stat(count / sum(count))*100),
+                 alpha = 0.5) +
+  geom_vline(xintercept = 0) +
+  scale_fill_identity() +
+  facet_wrap(~text_title, scales = "free") +
+  lims(x = c(-100, 100)) +
+  labs(x = "Residual (ŷ - y)", y = "Percentage") +
+  theme(strip.text = element_markdown(hjust = 0),
+        strip.background = element_blank())
+
+ggsave("figs/05_additional/02_model/01_distri-residuals_all.png", dpi = 600)
 
 # 6. Model interpretation ----
 
 ## 6.1 Variable importance ----
 
-model_results$result_vip %>% 
-  mutate(predictor = str_remove_all(predictor, "pred_")) %>% 
-  group_by(predictor, category) %>% 
-  summarise(importance = mean(importance)) %>% 
+### 6.1.1 Transform data ----
+
+data_imp_summary <- model_results$result_vip %>% 
+  mutate(predictor = str_remove_all(predictor, "pred_"),
+         predictor = str_replace_all(predictor, "ID_X", "ID_")) %>% 
+  group_by(predictor, category, color) %>% 
+  summarise(mean = mean(importance),
+            lower_ci_95 = quantile(importance, 0.05),
+            lower_ci_80 = quantile(importance, 0.10),
+            upper_ci_95 = quantile(importance, 0.95),
+            upper_ci_80 = quantile(importance, 0.80)) %>% 
   ungroup() %>% 
-  arrange(desc(importance)) %>% 
+  group_by(category, color) %>% 
+  arrange(desc(mean)) %>% 
   slice_head(n = 25) %>% 
-  ggplot(data = ., aes(x = fct_reorder(predictor, importance), y = importance)) +
-    geom_bar(stat = "identity") +
+  ungroup()
+
+data_imp_raw <- model_results$result_vip %>% 
+  mutate(predictor = str_remove_all(predictor, "pred_"),
+         predictor = str_replace_all(predictor, "ID_X", "ID_")) %>% 
+  left_join(data_imp_summary, .) %>% 
+  group_by(category)
+
+### 6.1.2 Create the function to make the plot ----
+
+plot_vimp <- function(category_i){
+  
+  data_imp_summary_i <- data_imp_summary %>% 
+    filter(category == category_i)
+  
+  data_imp_raw_i <- data_imp_raw %>% 
+    filter(category == category_i)
+  
+  plot_i <- ggplot(data = data_imp_summary_i, aes(x = fct_reorder(predictor, mean), color = color)) +
+    geom_jitter(data = data_imp_raw_i,
+                aes(x = fct_reorder(predictor, mean), y = importance, color = color),
+                alpha = 0.3, width = 0.15) +
+    geom_linerange(aes(ymin = lower_ci_95, ymax = upper_ci_95), linewidth = 1) +
+    geom_linerange(aes(ymin = lower_ci_80, ymax = upper_ci_80), linewidth = 1.5) +
+    geom_point(aes(y = mean), size = 3.5, shape = 21, fill = "white") +
     coord_flip() +
+    scale_color_identity() +
     labs(x = NULL, y = "Relative importance (%)")
+  
+  ggsave(filename = paste0("figs/04_supp/fig-3_", str_replace_all(str_to_lower(category_i), " ", "-"), ".png"),
+         plot = plot_i, height = 8, width = 4, dpi = 600)
+  
+}
+
+## 6.1.3 Map over the function ----
+
+map(unique(data_imp_summary$category), ~plot_vimp(category_i = .))
+
+
+
+
+
+
+
+
+
+
 
 ## 6.2 Partial Dependence Plot ----
 
+
+
+
+
+
 # 7. Benthic cover trends ----
 
-## 7.1 Transform the data ----
+## 7.1 Transform data ----
 
 data_trends <- model_results$result_trends %>% 
+  # Calculate mean and confidence interval
   rename(cover = mean) %>% 
-  group_by(category, region, territory, year) %>% 
+  group_by(category, region, territory, year, color, text_title) %>% 
   summarise(mean = mean(cover),
-            sd = sd(cover),
-            n = length(cover)) %>% 
-  ungroup() %>% 
-  mutate(t_score_95 = qt(p = 0.05/2, df = n, lower.tail = FALSE),
-         lower_ci_95 = mean - (t_score_95*(sd/sqrt(n))),
-         upper_ci_95 = mean + (t_score_95*(sd/sqrt(n))),
-         t_score_80 = qt(p = 0.2/2, df = n, lower.tail = FALSE),
-         lower_ci_80 = mean - (t_score_80*(sd/sqrt(n))),
-         upper_ci_80 = mean + (t_score_80*(sd/sqrt(n)))) %>% 
-  select(-sd, -n, -t_score_95, -t_score_80) %>% 
-  mutate(color = case_when(category == "Hard coral" ~ palette_second[2],
-                           category == "Coralline algae" ~ palette_second[3],
-                           category == "Macroalgae" ~ palette_second[4],
-                           category == "Turf algae" ~ palette_second[5]),
-         text_title = case_when(category == "Hard coral" ~ 
-                                  glue("**A.**<span style='color:{color}'> {category}</span>"),
-                                category == "Coralline algae" ~ 
-                                  glue("**B.**<span style='color:{color}'> {category}</span>"),
-                                category == "Macroalgae" ~ 
-                                  glue("**C.**<span style='color:{color}'> {category}</span>"),
-                                category == "Turf algae" ~ 
-                                  glue("**D.**<span style='color:{color}'> {category}</span>")),
-         category = as.factor(category),
-         category = fct_expand(category, "Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
-         category = fct_relevel(category, "Hard coral", "Coralline algae", "Macroalgae", "Turf algae"))
+            lower_ci_95 = quantile(cover, 0.05),
+            lower_ci_80 = quantile(cover, 0.10),
+            upper_ci_95 = quantile(cover, 0.95),
+            upper_ci_80 = quantile(cover, 0.80)) %>% 
+  ungroup()
 
-## 7.2 Pacific ----
+save(data_trends, file = "data/12_model-output/model_results_trends.RData")
 
-data_trends %>% 
-  filter(territory == "All") %>% 
-  ggplot(data = .) +
-  geom_ribbon(aes(x = year, ymin = lower_ci_95, ymax = upper_ci_95, fill = color), alpha = 0.3) +
-  geom_ribbon(aes(x = year, ymin = lower_ci_80, ymax = upper_ci_80, fill = color), alpha = 0.4) +
-  geom_line(aes(x = year, y = mean, color = color), linewidth = 1) +
-  scale_fill_identity() +
-  scale_color_identity() +
-  scale_x_continuous(expand = c(0, 0), limits = c(1980, NA)) +
-  labs(x = "Year", y = "Cover (%)") +
-  theme(plot.title = element_markdown(size = 12))
-
-## 7.3 Countries and territories ----
-
-data_trends %>% 
-  filter(territory != "All") %>% 
-  ggplot(data = .) +
-  geom_ribbon(aes(x = year, ymin = lower_ci_95, ymax = upper_ci_95, fill = color), alpha = 0.3) +
-  geom_ribbon(aes(x = year, ymin = lower_ci_80, ymax = upper_ci_80, fill = color), alpha = 0.4) +
-  geom_line(aes(x = year, y = mean, color = color), linewidth = 1) +
-  scale_fill_identity() +
-  scale_color_identity() +
-  scale_x_continuous(expand = c(0, 0), limits = c(1980, NA)) +
-  facet_wrap(~territory, scales = "free_y") +
-  labs(x = "Year", y = "Cover (%)") +
-  theme(plot.title = element_markdown(size = 12))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 4. Temporal trends of benthic cover (territory) ----
-
-## 4.1 Calculate confidence interval ----
-
-data_trends <- results_all_raw$results_region %>% 
-  mutate(territory = "Pacific") %>% 
-  bind_rows(., results_all_raw$results_territory) %>% 
-  rename(cover = mean) %>% 
-  group_by(category, territory, year) %>% 
-  summarise(mean = mean(cover),
-            sd = sd(cover),
-            n = length(cover)) %>% 
-  ungroup() %>% 
-  mutate(t_score_95 = qt(p = 0.05/2, df = n, lower.tail = FALSE),
-         lower_ci_95 = mean - (t_score_95*(sd/sqrt(n))),
-         upper_ci_95 = mean + (t_score_95*(sd/sqrt(n))),
-         t_score_80 = qt(p = 0.2/2, df = n, lower.tail = FALSE),
-         lower_ci_80 = mean - (t_score_80*(sd/sqrt(n))),
-         upper_ci_80 = mean + (t_score_80*(sd/sqrt(n)))) %>% 
-  select(-sd, -n, -t_score_95, -t_score_80)
-
-save(data_trends, file = "data/16_model-data/final-results.RData")
-
-## 4.2 Add the colors ----
-
-data_trends <- data_trends %>% 
-  mutate(color = case_when(category == "Hard coral" ~ palette_second[2],
-                           category == "Coralline algae" ~ palette_second[3],
-                           category == "Macroalgae" ~ palette_second[4],
-                           category == "Turf algae" ~ palette_second[5]),
-         text_title = case_when(category == "Hard coral" ~ 
-                                  glue("**A.**<span style='color:{color}'> {category}</span>"),
-                                category == "Coralline algae" ~ 
-                                  glue("**B.**<span style='color:{color}'> {category}</span>"),
-                                category == "Macroalgae" ~ 
-                                  glue("**C.**<span style='color:{color}'> {category}</span>"),
-                                category == "Turf algae" ~ 
-                                  glue("**D.**<span style='color:{color}'> {category}</span>")),
-         category = as.factor(category),
-         category = fct_relevel(category, "Hard coral", "Coralline algae", "Macroalgae", "Turf algae"))
-
-## 4.3 Create the function to make a plot ----
+## 7.2 Create the function to make a plot ----
 
 plot_trends <- function(category_i, data_trends_i){
   
   data_trends_j <- data_trends_i %>% 
     filter(category == category_i)
   
-  plot_i <- ggplot(data = data_trends_j) +
+  plot_j <- ggplot(data = data_trends_j) +
     geom_ribbon(aes(x = year, ymin = lower_ci_95, ymax = upper_ci_95, fill = color), alpha = 0.3) +
     geom_ribbon(aes(x = year, ymin = lower_ci_80, ymax = upper_ci_80, fill = color), alpha = 0.4) +
     geom_line(aes(x = year, y = mean, color = color), linewidth = 1) +
@@ -270,11 +267,11 @@ plot_trends <- function(category_i, data_trends_i){
     labs(x = "Year", y = "Cover (%)", title = unique(data_trends_j$text_title)) +
     theme(plot.title = element_markdown(size = 12))
   
-  plot_i
+  return(plot_j)
   
 }
 
-## 4.4 Create the function to combine the plots ----
+## 7.3 Create the function to combine the plots ----
 
 combine_plot_trends <- function(territory_i){
   
@@ -286,7 +283,7 @@ combine_plot_trends <- function(territory_i){
   
   plot_i <- wrap_plots(plot_list, ncol = 1)
   
-  if(territory_i == "Pacific"){
+  if(territory_i == "All"){
     
     ggsave(filename = "figs/01_part-1/fig-12.png", plot = plot_i, height = 12, width = 4, dpi = 600)  
     
@@ -299,40 +296,74 @@ combine_plot_trends <- function(territory_i){
   
 }
 
-## 4.5 Map over the function ----
+## 7.4 Map over the function ----
 
 map(unique(data_trends$territory), ~combine_plot_trends(territory_i = .))
 
-# 5. Temporal trends of benthic cover (category) ----
 
-## 5.1 Create the function ----
 
-plot_trends <- function(category_i){
-  
-  data_trends_i <- data_trends %>% 
-    filter(category == category_i) %>% 
-    filter(territory != "Pacific")
-  
-  plot_i <- ggplot(data = data_trends_i) +
-    geom_ribbon(aes(x = year, ymin = lower_ci_95, ymax = upper_ci_95, fill = color), alpha = 0.3) +
-    geom_ribbon(aes(x = year, ymin = lower_ci_80, ymax = upper_ci_80, fill = color), alpha = 0.4) +
-    geom_line(aes(x = year, y = mean, color = color), linewidth = 1) +
-    scale_fill_identity() +
-    scale_color_identity() +
-    facet_wrap(~territory) +
-    scale_x_continuous(expand = c(0, 0), limits = c(1980, NA)) +
-    labs(x = "Year", y = "Cover (%)") +
-    theme(strip.background = element_blank())
-  
-  ggsave(filename = paste0("figs/05_additional/03_results/comparison-territories_", 
-                           str_replace_all(str_to_lower(category_i), " ", "-"), ".png"),
-         plot = plot_i, height = 15, width = 20)
-  
-}
 
-## 5.2 Map over the function ----
 
-map(unique(data_trends$category), ~plot_trends(category_i = .))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # 6. Variable importance ----
 
@@ -397,6 +428,15 @@ plot_vip <- function(category_i){
 ## 6.2 Map over the function ----
 
 map(unique(results_all_raw$result_vip$category), ~plot_vip(category_i = .))
+
+
+
+
+
+
+
+
+
 
 
 
@@ -593,24 +633,4 @@ ggplot(data = A, aes(x = territory, y = rsq)) +
   geom_point() +
   coord_flip() +
   labs(x = NULL, y = "RMSE")
-
-# Pred. vs Obs.
-list$result_pred_obs %>% 
-  ggplot(data = ., aes(x = y, y = yhat)) +
-  geom_point(alpha = 0.1) +
-  geom_abline(slope = 1) +
-  labs(x = "Observed value (y)", y = "Predicted value (ŷ)")
-
-# Distribution residuals
-list$result_pred_obs %>% 
-  mutate(residual = yhat - y) %>% 
-  ggplot(data = ., aes(x = residual)) + 
-  geom_histogram(aes(y = after_stat(count / sum(count))*100),
-                 alpha = 0.5) +
-  geom_vline(xintercept = 0) +
-  lims(x = c(-100, 100)) +
-  labs(x = "Residual (ŷ - y)", y = "Percentage")
-
-
-
 
