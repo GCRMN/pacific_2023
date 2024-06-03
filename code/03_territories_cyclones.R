e52e9f68d7ae91ec2900fd980141c9b87cf965d0
JWicquart <jeremywicquart@gmail.com>
2024-06-03 22:24:11 +0200

    put legend on the right

--- code/03_territories_cyclones.R
+++ code/03_territories_cyclones.R
@@ -423,29 +423,31 @@ map_cyclone_plot <- function(territory_i){
                      aes(label = name, color = max_saffir), fill = "white", alpha = 0.9,
                      label.r = unit(0.4, "lines"), show.legend = FALSE) +
     scale_y_continuous(breaks = c(0, 50 ,100, 150, 200, 250, 300), limits = c(0, 300)) +
-    scale_x_date(expand = c(0, 0), limits = c(ymd("1980-01-01"), ymd("2030-01-01"))) +
+    scale_x_date(limits = c(ymd("1980-01-01"), ymd("2024-01-01"))) +
     coord_cartesian(ylim = c(14.25, 310)) +
     scale_fill_manual(breaks = c("1", "2", "3", "4", "5"),
                       labels = c("Cat. 1", "Cat. 2", "Cat. 3", "Cat. 4", "Cat. 5"),
                       values = c(palette_second[2:5], "black"),
-                      name = "Saffir-Simpson category",
+                      name = "Saffir-Simpson\ncategory",
                       drop = FALSE) +
     scale_color_manual(breaks = c("1", "2", "3", "4", "5"),
                        labels = c("Cat. 1", "Cat. 2", "Cat. 3", "Cat. 4", "Cat. 5"),
                        values = c(palette_second[2:5], "black"),
-                       name = "Saffir-Simpson category",
+                       name = "Saffir-Simpson\ncategory",
                        drop = FALSE) +
-    guides(fill = guide_legend(title.position = "top", title.hjust = 0.5, override.aes = list(size = 4))) +
+    guides(fill = guide_legend(override.aes = list(size = 4))) +
     labs(x = "Year", y = bquote("Wind speed (km."~h^-1*")")) +
     theme_graph() +
     theme(text = element_text(size = 13),
+          legend.position = "right",
+          legend.direction = "vertical",
           axis.title.x = element_text(size = 14),
           axis.title.y = element_text(size = 14))
   
   # 3. Save the plot
   
   ggsave(filename = paste0("figs/02_part-2/fig-4/", str_replace_all(str_to_lower(territory_i), " ", "-"), ".png"),
-         plot = plot_i, height = 4, width = 8, dpi = fig_resolution)
+         plot = plot_i, height = 3.5, width = 9, dpi = fig_resolution)
   
 }
 
