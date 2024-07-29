55fe4cd71f86fb885c47a3042cd5bc2dd837a2c8
JWicquart <jeremywicquart@gmail.com>
2024-07-29 09:10:11 +0200

    replace soi by nino 3.4 for predictor

--- code/c4_extract_predictor_enso.R
+++ code/c4_extract_predictor_enso.R
@@ -5,27 +5,15 @@ library(RcppRoll)
 
 # 2. Load and transform SOI data ----
 
-read_table("data/09_misc/enso-soi.txt", skip = 87) %>% 
-  filter(YEAR %in% c(1980:2023)) %>% 
-  select(-X14) %>% 
-  mutate_all(., ~as.numeric(.)) %>% 
-  pivot_longer(2:ncol(.), values_to = "soi", names_to = "month") %>% 
-  rename(year = YEAR) %>% 
-  mutate(month = str_replace_all(month, c("JAN" = "1",
-                                          "FEB" = "2",
-                                          "MAR" = "3",
-                                          "APR"= "4",
-                                          "MAY"= "5",
-                                          "JUN" = "6",
-                                          "JUL"= "7",
-                                          "AUG" = "8",
-                                          "SEP" = "9",
-                                          "OCT" = "10",
-                                          "NOV" = "11",
-                                          "DEC"= "12")),
+data_enso <- read_table("data/09_misc/nino34.long.anom.data.txt", skip = 1, col_names = FALSE, n_max = 154) %>% 
+  rename(year = 1) %>% 
+  filter(year >= 1980 & year <= 2023) %>% 
+  pivot_longer(2:ncol(.), values_to = "nino", names_to = "month") %>% 
+  mutate(month = str_remove_all(month, "X"),
+         month = as.numeric(month)-1,
          date = ym(paste(year, month, sep = "-"))) %>% 
   # Mean of SOI value over the last 24 months (i.e. 2 years)
-  mutate(enso = roll_mean(x = soi, n = 24, align = "right", fill = NA)) %>% 
+  mutate(enso = roll_mean(x = nino, n = 24, align = "right", fill = NA)) %>% 
   filter(month == "12") %>% 
   select(year, enso) %>% 
   rename(pred_enso = enso) %>% 
