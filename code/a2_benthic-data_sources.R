c8bfa320217ae4da135c501e2a68350f2b5bf9f7
JWicquart <jeremywicquart@gmail.com>
2024-12-22 11:28:07 +0100

    update author's contribution

--- code/a2_benthic-data_sources.R
+++ code/a2_benthic-data_sources.R
@@ -6,6 +6,10 @@ library(sf)
 
 # 2. Load data ----
 
+data_eez <- st_read("data/01_background-shp/03_eez/data_eez.shp") %>% 
+  rename(territory = TERRITORY1) %>% 
+  select(territory)
+
 load("data/09_misc/data-benthic.RData")
 
 # 3. DatasetID per territory ----
@@ -16,48 +20,42 @@ data_benthic %>%
   arrange(territory, datasetID) %>% 
   group_by(territory) %>% 
   summarise(datasetID = paste0(datasetID, collapse = ", ")) %>% 
-  write.csv2(., file = "figs/03_methods/table-1.csv",
-             row.names = FALSE)
-
-# 4. Extract data sources for datasets used in the analyses ----
-
-read_xlsx(path = "../../2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/05_data-sources.xlsx",
-          sheet = 1) %>% 
-  filter(datasetID %in% unique(data_benthic$datasetID)) %>% 
-  write.csv2(., file = "figs/04_supp/data-sources.csv",
-             row.names = FALSE)
-
-# 5. Data contributors per territory ----
-
-data_benthic %>% 
-  select(datasetID, country, territory) %>% 
-  distinct() %>% 
-  full_join(., read_xlsx(path = "../../2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/05_data-sources.xlsx",
-                         sheet = 1) %>% 
-              filter(datasetID %in% unique(data_benthic$datasetID)) %>% 
-              select(datasetID, last_name, first_name, email)) %>% 
-  select(-datasetID, -country) %>% 
-  distinct() %>% 
-  bind_rows(., read_xlsx(path = "data/09_misc/non-data-contributors_authors.xlsx")) %>% 
+  left_join(data_eez %>% st_drop_geometry(), .) %>% 
   arrange(territory) %>% 
-  write.csv2(., file = "figs/04_supp/contributors-territory.csv",
-             row.names = FALSE)
-  
-# 6. DatasetID per territory ----
+  openxlsx::write.xlsx(., file = "figs/04_supp/tbl-1.xlsx")
 
-data_eez <- st_read("data/01_background-shp/03_eez/data_eez.shp") %>% 
-  st_drop_geometry() %>% 
-  select(TERRITORY1) %>% 
-  rename(territory = TERRITORY1)
+# 4. List of contributors per datasetID ----
 
-data_benthic %>% 
-  select(territory, datasetID) %>% 
+read_xlsx("C:/Users/jwicquart/Desktop/Recherche/03_projects/2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/05_data-sources.xlsx") %>% 
+  filter(datasetID %in% unique(data_benthic$datasetID)) %>% 
+  select(datasetID, rightsHolder, last_name, first_name, email) %>% 
+  openxlsx::write.xlsx(., file = "figs/04_supp/tbl-2.xlsx")
+
+# 5. List of contributors emails ----
+
+read_xlsx("C:/Users/jwicquart/Desktop/Recherche/03_projects/2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/05_data-sources.xlsx") %>% 
+  filter(datasetID %in% unique(data_benthic$datasetID)) %>% 
+  select(last_name, first_name, email) %>% 
   distinct() %>% 
-  # Add territories with no data
-  left_join(data_eez, .) %>% 
+  arrange(last_name) %>% 
+  openxlsx::write.xlsx(., file = "figs/04_supp/tbl-3.xlsx")
+
+# 6. List of contributors per area ----
+
+read_xlsx("C:/Users/jwicquart/Desktop/Recherche/03_projects/2022-02-10_gcrmndb_benthos/gcrmndb_benthos/data/05_data-sources.xlsx") %>% 
+  filter(datasetID %in% unique(data_benthic$datasetID)) %>% 
+  select(datasetID, last_name, first_name) %>% 
+  full_join(data_benthic %>% 
+              select(datasetID, territory) %>% 
+              distinct(),
+            .) %>% 
+  drop_na(last_name) %>% 
+  arrange(territory, last_name) %>% 
+  mutate(last_name = str_to_title(last_name),
+         name = paste0(first_name, " ", last_name)) %>% 
+  select(-datasetID, -last_name, -first_name) %>% 
+  distinct() %>% 
   group_by(territory) %>% 
-  summarise(datasetID = paste0(datasetID, collapse = ", ")) %>% 
-  ungroup() %>% 
-  arrange(territory, datasetID) %>% 
-  write.csv2(., file = "figs/04_supp/datasetid-territory.csv",
-             row.names = FALSE)
+  mutate(name = paste0(name, collapse = ", ")) %>% 
+  distinct() %>% 
+  openxlsx::write.xlsx(., file = "figs/04_supp/tbl-4.xlsx")
---
+++ code/e02_authors-contribution.R
@@ -0,0 +1,116 @@
+# 1. Load packages ----
+
+library(tidyverse)
+library(readxl)
+library(googledrive)
+library(openxlsx)
+
+# 2. Create the tibble ----
+
+data_contribution <- read_xlsx("figs/04_supp/tbl-2.xlsx") %>% 
+  drop_na(first_name) %>% # Remove NA (datasets from paper) 
+  select(first_name, last_name, email) %>% 
+  distinct() %>% 
+  mutate(`Data acquisition` = "X") %>% 
+  # Add contributors who are not data contributors
+  full_join(., read_xlsx("figs/04_supp/tbl-5.xlsx")) %>% 
+  # Create all the columns
+  mutate(`Funding acquisition` = NA,
+         `Supervision` = NA,
+         `Conceptualization` = NA,
+         `Facilitation` = NA,
+         `Data integration` = NA,
+         `Data analysis` = NA,
+         `Participation to workshop` = NA,
+         `Executive summary` = NA,
+         `Synthesis for the region` = NA,
+         `Syntheses for count. and terr.` = NA,
+         `Case studies` = NA,
+         `Materials and Methods` = NA,
+         `Layout` = NA,
+         `Communication` = NA) %>% 
+  relocate(`Data acquisition`, .before = `Data integration`) %>% 
+  arrange(last_name, .locale = "en") %>% 
+  mutate(across(c(first_name, last_name), ~str_squish(.x))) %>% 
+  # Concatenate email address
+  group_by(first_name, last_name) %>% 
+  mutate(email = paste0(email, collapse = ", ")) %>% 
+  ungroup() %>% 
+  distinct()
+  
+# 3. Export the file ----
+
+## 3.1 Create the function to export the file with Excel formatting ----
+
+export_contributions <- function(data, path = "figs/00_misc/authors_contribution.xlsx"){
+
+  wb <- createWorkbook()
+  
+  addWorksheet(wb, "Sheet1")
+  
+  addStyle(wb, "Sheet1",
+           cols = 1,
+           rows = seq(from = 2, to = nrow(data_contribution)+1, by = 1),
+           style = createStyle(textDecoration = "Bold", halign = "right"))
+  
+  addStyle(wb, "Sheet1",
+           cols = 2,
+           rows = seq(from = 2, to = nrow(data_contribution)+1, by = 1),
+           style = createStyle(textDecoration = "Bold", halign = "left"))
+  
+  addStyle(wb, "Sheet1",
+           cols = seq(from = 3, to = ncol(data_contribution), by = 1),
+           rows = seq(from = 2, to = nrow(data_contribution)+1, by = 1),
+           style = createStyle(halign = "center"),
+           gridExpand = TRUE)
+  
+  writeData(wb = wb,
+            sheet = "Sheet1",
+            x = data,
+            headerStyle = createStyle(textDecoration = "Bold", textRotation = 90, halign = "center"))
+  
+  saveWorkbook(wb, path, overwrite = TRUE)
+
+}
+
+## 3.2 Export the file or update it ----
+
+if(file.exists("figs/00_misc/authors_contribution.xlsx") == FALSE){
+  
+  export_contributions(data = data_contribution)
+  
+}else{
+  
+  data_contribution_before <- read_xlsx("figs/00_misc/authors_contribution.xlsx")
+  
+  data_contribution <- data_contribution %>% 
+    select(first_name, last_name, `Data acquisition`) %>% 
+    left_join(., data_contribution_before) %>% 
+    relocate(`Data acquisition`, .before = `Data integration`)
+  
+  export_contributions(data = data_contribution)
+  
+}
+
+# ----------------------------------------------------------------------------------- #
+# /!\     FILL THE EXPORTED FILE WITH AUTHOR'S CONTRIBUTIONS BUT:                 /!\ #
+# /!\        1) ROWS MUST NOT BE ADDED MANUALLY                                   /!\ #
+# /!\        2) THE COLUMN DATA ACQUISITION MUST NOT BE CHANGED                   /!\ #
+# /!\        3) MODIFICATIONS MUST BE DONE ON LOCAL FILE NOT ON GOOGLE DRIVE      /!\ #
+# ----------------------------------------------------------------------------------- #
+
+# 4. Export to Google Drive (without email) ----
+
+data_contribution <- read_xlsx("figs/00_misc/authors_contribution.xlsx")
+
+data_contribution <- data_contribution %>% 
+  select(-email)
+
+export_contributions(data = data_contribution, path = "figs/00_misc/authors_contribution_temp.xlsx")
+
+drive_put(media = "figs/00_misc/authors_contribution_temp.xlsx",
+          path = "GCRMN Pacific report/10_authors-contribution.xlsx")
+
+# Select Google account before running the last line of code
+
+file.remove("figs/00_misc/authors_contribution_temp.xlsx")
