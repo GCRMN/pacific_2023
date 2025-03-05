1db4bc9ca2b7cedc8bc6dcf63df4b6534a9095b5
JWicquart <jeremywicquart@gmail.com>
2025-03-05 18:03:07 +0100

    major benthic categories plots for pria and kiribati

--- code/function/combine_plot_trends.R
+++ code/function/combine_plot_trends.R
@@ -24,6 +24,132 @@ combine_plot_trends <- function(territory_i, categ_type){
       
       ggsave(filename = "figs/01_part-1/fig-13.png", plot = plot_i, height = 7, width = 9, dpi = fig_resolution)  
       
+    }else if(territory_i %in% c("Gilbert Islands", "Line Group", "Phoenix Group")){
+      
+      data_trends_i <- data_trends$smoothed_trends %>% 
+        filter(year >= 1990 & year <= 2022) %>% 
+        filter(territory_i %in% c("Gilbert Islands", "Line Group", "Phoenix Group")) %>% 
+        filter(category %in% c("Hard coral", "Coralline algae", "Macroalgae",
+                               "Turf algae")) %>% 
+        mutate(category = as.factor(category),
+               category = fct_expand(category, "Hard coral", "Coralline algae", "Macroalgae",
+                                     "Turf algae"),
+               category = fct_relevel(category, "Hard coral", "Coralline algae", "Macroalgae",
+                                      "Turf algae"))
+      
+      max_y <- max(as.numeric(data_trends_i$upper_ci_95), na.rm = TRUE)
+      
+      plot_list1 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Gilbert Islands"),
+                                     show_obs_data = "ribbon", max_y = max_y))
+      
+      plot_list2 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Line Group"),
+                                     show_obs_data = "ribbon", max_y = max_y))
+      
+      plot_list3 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Phoenix Group"),
+                                     show_obs_data = "ribbon", max_y = max_y)) 
+      
+      plot_i <- wrap_plots(wrap_plots((ggplot() + theme_void() + labs(title = "1. Gilbert Islands") +
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list1, ncol = 4), 
+                           wrap_plots((ggplot() + theme_void() + labs(title = "2. Line Group") + 
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list2, ncol = 4),
+                           wrap_plots((ggplot() + theme_void() + labs(title = "3. Phoenix Group") + 
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list3, ncol = 4),
+                           nrow = 6) +
+        plot_layout(heights = c(0.1, 1, 0.1, 1, 0.1, 1))
+      
+      ggsave(filename = "figs/02_part-2/fig-6/kiribati.png", plot = plot_i, height = 11, width = 13, dpi = fig_resolution)
+      
+    }else if(territory_i %in% c("Howland and Baker Islands", "Jarvis Island", "Johnston Atoll",
+                                "Palmyra Atoll", "Wake Island")){
+      
+      data_trends_i <- data_trends$smoothed_trends %>% 
+        filter(year >= 1990 & year <= 2022) %>% 
+        filter(territory_i %in% c("Howland and Baker Islands", "Jarvis Island", "Johnston Atoll",
+                                  "Palmyra Atoll", "Wake Island")) %>% 
+        filter(category %in% c("Hard coral", "Coralline algae", "Macroalgae",
+                               "Turf algae")) %>% 
+        mutate(category = as.factor(category),
+               category = fct_expand(category, "Hard coral", "Coralline algae", "Macroalgae",
+                                     "Turf algae"),
+               category = fct_relevel(category, "Hard coral", "Coralline algae", "Macroalgae",
+                                      "Turf algae"))
+      
+      max_y <- max(as.numeric(data_trends_i$upper_ci_95), na.rm = TRUE)
+      
+      plot_list1 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Howland and Baker Islands"),
+                                     show_obs_data = "ribbon", max_y = max_y))
+      
+      plot_list2 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Johnston Atoll"),
+                                     show_obs_data = "ribbon", max_y = max_y))
+      
+      plot_list3 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Palmyra Atoll"),
+                                     show_obs_data = "ribbon", max_y = max_y))
+      
+      plot_list4 <- map(c("Hard coral", "Coralline algae", "Macroalgae", "Turf algae"),
+                        ~plot_trends(category_i = ., data_trends_i = data_trends_i %>%
+                                       filter(territory == "Wake Island"),
+                                     show_obs_data = "ribbon", max_y = max_y))
+      
+      plot_i <- wrap_plots(wrap_plots((ggplot() + theme_void() + labs(title = "1. Howland and Baker Islands") +
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list1, ncol = 4), 
+                           wrap_plots((ggplot() + theme_void() + labs(title = "2. Johnston Atoll") + 
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list2, ncol = 4),
+                           wrap_plots((ggplot() + theme_void() + labs(title = "3. Palmyra Atoll") +
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list3, ncol = 4),
+                           wrap_plots((ggplot() + theme_void() + labs(title = "4. Wake Island") +
+                                         theme_graph() + theme(plot.title = element_text(hjust = 0, face = "bold"),
+                                                               plot.margin = margin(30, 0, 0, 0))),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(),
+                                      ggplot() + theme_void(), nrow = 1),
+                           wrap_plots(plot_list4, ncol = 4),
+                           nrow = 10) +
+        plot_layout(heights = c(0.1, 1, 0.1, 1, 0.1, 1, 0.1, 1, 0.1, 1))
+      
+      ggsave(filename = "figs/02_part-2/fig-6/pria.png", plot = plot_i, height = 14.5, width = 13, dpi = fig_resolution)
+      
     }else{
       
       ggsave(filename = paste0("figs/02_part-2/fig-6/",
